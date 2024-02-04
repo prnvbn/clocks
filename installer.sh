658cb3031aa3dbcb82a93fc20b117327e14a55ab
Pranav Bansal <prnvbn@gmail.com>
2024-02-04 19:49:10 +0000

    BUGFIX installer script

--- installer.sh
+++ installer.sh
@@ -30,7 +30,7 @@ curl -s https://api.github.com/repos/prnvbn/clocks/releases/latest \
 | grep "browser_download_url" \
 | cut -d : -f 2,3 \
 | tr -d \" \
-| grep darwin \
+| grep "$PLATFORM" \
 | xargs -I{} curl -o clocks -sL {}
 
 chmod +x clocks
